ef3122003af5c69d4220b9ccf2d8b4089a387c77
unknown <alex.badamtchi@gmail.com>
2021-11-14 22:17:03 +0300

    In Progress

--- Canon.py
+++ Canon.py
@@ -175,3 +175,40 @@ class Target(GameObject):
         pass
 
 
+class MovingTarget(Target):
+    def __init__(self, coord=None, color=None, rad=30):
+        super().__init__(color, coord, rad)
+        self.vx = randint(-2, +2)
+        self.vy = randint(-2, +2)
+
+    def move(self):
+        self.coord[0] += self.vx
+        self.coord[1] += self.vy
+
+
+class ScoreTable:
+    """
+    Score Table class.
+    """
+    def __init__(self, t_destr=0, b_used=0):
+        self.t_destr = t_destr
+        self.b_used = b_used
+        self.font = pg.font.SysFont("dejavusansmono", 25)
+
+    def score(self):
+        """
+        Score calculation method.
+        """
+        return self.t_destr - self.b_used
+
+    def draw(self, screen):
+        score_surf = []
+        score_surf.append(self.font.render("Destroyed: {}".format(self.t_destr), True, WHITE))
+        score_surf.append(self.font.render("Balls used: {}".format(self.b_used), True, WHITE))
+        score_surf.append(self.font.render("Total: {}".format(self.score()), True, RED))
+        for i in range(3):
+            screen.blit(score_surf[i], [10, 10 + 30*i])
+
+
+class Manager:
+    
