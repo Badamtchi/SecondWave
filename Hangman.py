9d1a3647a444d3eb749e833c5183d62476f4d9e3
Alex Badamtchi <alex.badamtchi@gmail.com>
2021-11-23 03:18:17 +0300

    Hangman Exercise
    In progress

---
+++ Hangman.py
@@ -0,0 +1,30 @@
+import random
+import string
+from words import words
+
+def get_valid_word(words):
+    word = random.choice(words) # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word) # letters in word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() # what the user has guessed
+
+    # getting user input
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print('Sorry! You have already usec this charachter!\nPlease try again...')
+
+    else:
+        print('Invalid characterA\nPlease try again...')
+
+
