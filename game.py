2561980f594112712cdd08571d3aeef6071bb172
Alex Badamtchi <alex.badamtchi@gmail.com>
2021-11-25 02:50:29 +0300

    Game rules of TicTacToe
    linked to TicTacToe.py
    Nov 2021

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+import time
+from TicTacToe import HumanPlayer, RandomComputerPlayer
+
 class TicTacToe:
     def __init__(self):
         self.board = [' ' for _ in range(9)] # we will use a single list to rep 3x3 board
@@ -47,10 +50,19 @@ class TicTacToe:
         if all(spot == letter for spot in column):
             return True
 
-        # check diognal
-        
+        # check diognals
+        # but only if the square is an even number (0, 2, 4, 6, 8)
+        # these are the only moves possible to win a diagonal
+        if square % 2 ==0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]] # left to right diagonal
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]] # right to left diagonal
+            if all([spot == letter for spot in diagonal2]):
+                return True
 
-
+        # if all of these fail
+        return False
 
 
 def play(game, x_player, o_player, print_game=True):
@@ -86,6 +98,14 @@ def play(game, x_player, o_player, print_game=True):
             # after we made our move, we need to alternate letters
             letter = 'O' if letter == 'X' else 'X'
 
-        if print_game:
-            print('It\'s a tie! ')
+        # tiny break to make things a little easier to read
+        time.sleep(0.8)
 
+    if print_game:
+        print('It\'s a tie! ')
+
+if __name__ == '__main__':
+    x_player = HumanPlayer('X')
+    o_player = RandomComputerPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
