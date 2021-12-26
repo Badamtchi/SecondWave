3779940b01e52b818c0076cf6fb11c7cd373dab2
Alex Badamtchi <alex.badamtchi@gmail.com>
2021-12-26 22:58:18 +0300

    MachinState

--- MachineState.py
+++ MachineState.py
@@ -6,3 +6,12 @@ class State(ABC):
     def in_state(self):
         pass
 
+class HappyState(State):
+
+    def in_state(self):
+        print("I am in HAPPY state!")
+
+class SadState(State):
+
+    def in_state(self):
+        print("I am in Sad state!")
