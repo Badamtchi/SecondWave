894756a496a2f57417dbdfb06a700c630b79116b
Alex Badamtchi <alex.badamtchi@gmail.com>
2021-12-28 11:56:21 +0300

    Trading Bot, init args 002
    Strategy Pattern Exercises, OOP
    DEC 2021

--- StrategyParametes_002.py
+++ StrategyParametes_002.py
@@ -17,3 +17,74 @@ class TradingStrategy(ABC):
     @abstractmethod
     def should_sell(self, prices: list[float]) -> bool:
         """Whether you should sell this coin, given the prices."""
+
+
+@dataclass
+class AverageTradingStrategy(TradingStrategy):
+    """Trading strategy based on price averages."""
+
+    window_size: int = 3
+
+    def should_buy(self, prices: list[float]) -> bool:
+        list_window = prices[-int(self.window_size) :]
+        return prices[-1] < statistics.mean(list_window)
+    
+    def should_sell(self, prices: list[float]) -> bool:
+        list_window = prices[-int(self.window_size) :]
+        return prices[-1] > statistics.mean(list_window)
+
+
+@dataclass
+class MinMaxTradingStrategy(TradingStrategy):
+    """Trading strategy based on price minima and maxima."""
+
+    min_price: float = 32000.0
+    max_price: float = 33000.0
+
+    def should_buy(self, prices: list[float]) -> bool:
+        # buy if it's below the min_price.
+        return prices[-1] < self.min_price
+
+    def should_sell(self, prices: list[float]) -> bool:
+        # sell if it's above the max_price.
+        return prices[-1] > self.max_price
+
+
+class TradingBot:
+    """Trading bot that connects to a crypto exchange and performs trades."""
+
+    def __init__(self, exchange: Exchange, trading_strategy: TradingStrategy) -> None:
+        self.exchange = exchange
+        self.trading_strategy = trading_strategy
+
+    def run(self, symbol: str):
+        """Run the trading bot once for a perticular symbol, with a given strategy."""
+        prices = self.exchange.get_market_data(symbol)
+        should_buy = self.trading_strategy.should_buy(prices)
+        should_sell = self.trading_strategy.should_sell(prices)
+        if should_buy:
+            self.exchange.buy(symbol, 10)
+        elif should_sell:
+            self.exchange.sell(symbol, 10)
+        else:
+            print(f"No action needed for {symbol}")
+
+def main():
+    """
+    Create an exchange and a trading bot with a strategy.
+    Run the strategy once on a paticular symbol.
+    """
+
+    # create the exchange and connect to it
+    exchange = Exchange()
+    exchange.connect()
+
+    # create the trading strategy
+    trading_strategy = MinMaxTradingStrategy(min_price=31000, max_price=34000)
+
+    # create the trading bot and run the bot once
+    bot = TradingBot(exchange, trading_strategy)
+    bot.run("BTC/USD")
+
+if __name__ == "__main__":
+    main()
