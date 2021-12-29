4a67e7d0c3e7245910a73e53ed42f846f76175f3
Alex Badamtchi <alex.badamtchi@gmail.com>
2021-12-29 17:17:27 +0300

    Trading Bot

--- StrategyParametes_003.py
+++ StrategyParametes_003.py
@@ -37,3 +37,39 @@ class AverageTradingStrategy(TradingStrategy):
         list_window = prices[-params.window_size :]
         return prices[-1] > statistics.mean(list_window)
 
+class MinMaxTradingStrategy(TradingStrategy):
+    """Trading strategy based on price minima and maxima."""
+
+    def should_buy(self, prices: list[float], params: StrategyParameters) -> bool:
+        # buy if it's below the min_price.
+        return prices[-1] < params.min_price
+    
+    def should_sell(self, prices: list[float], params: StrategyParameters) -> bool:
+        # sell if it's above the max_price.
+        return prices[-1] > params.max_price
+
+
+class TradingBot:
+    """Trading bot that connects to a crypto exchange and performs trades."""
+
+    def __init__(self, exchange: Exchange, trading_strategy: TradingStrategy) -> None:
+        self.exchange = exchange
+        self.trading_strategy = trading_strategy
+    
+    def run(self, symbol: str):
+        """Run the trading bot once for a particular symbol, with a given strategy"""
+        prices = self.trading_strategy.get_market_data(symbol)
+        should_buy = self.trading_strategy.should_buy(
+            prices, StrategyParameters(min_price=31000)
+        )
+        should_sell = self.trading_strategy.should_sell(
+            prices, StrategyParameters(max_price=33000)
+        )
+        if should_buy:
+            self.exchange.buy(symbol, 10)
+        elif should_sell:
+            self.exchange.sell(symbol, 10)
+        else:
+            print(f"No action needed for {symbol}")
+
+
